7828cd78455f813afab08b5ce07b10688d327ad7
Runzhi Fan <74864699+ranger2571@users.noreply.github.com>
2024-09-21 15:42:46 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,17 +7,14 @@ from sklearn.metrics import classification_report, confusion_matrix
 
 # 从Excel表格中读取数据，假设文件名为 'core_loss_data.xlsx'
 data = pd.read_excel('001.xlsx')
-print("1")
 # 提取温度、频率、核心损耗和励磁波形类型列
 temperature = data.iloc[:, 0]
 frequency = data.iloc[:, 1]
 core_loss = data.iloc[:, 2]
 excitation_waveform = data.iloc[:, 3]
-print("2")
 # 提取磁通密度列（第5列到第1029列，即磁通密度 flux_density_1 到 flux_density_1024）
 flux_density_columns = data.columns[4:1029]
 flux_density_data = data[flux_density_columns]
-print("2")
 # 特征提取函数
 def extract_features(flux_density):
     features = {
@@ -30,71 +27,24 @@ def extract_features(flux_density):
         'kurtosis': flux_density.kurtosis()
     }
     return pd.Series(features)
-
 # 应用特征提取到磁通密度数据
 features = flux_density_data.apply(extract_features, axis=1)
-print("4")
 # 合并温度、频率、核心损耗数据到提取的特征中
 #features['temperature'] = temperature
 #features['frequency'] = frequency
 #features['core_loss'] = core_loss
-print("5")
 # 准备目标变量：励磁波形类型
 label_encoder = LabelEncoder()
 labels = label_encoder.fit_transform(excitation_waveform)
-print("6")
 # 划分训练集和测试集
 X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
-
 # 训练SVM模型
 svm_model = SVC(kernel='rbf')
 svm_model.fit(X_train, y_train)
-print("7")
 # 进行预测
 y_pred1 = svm_model.predict(X_test)
-
 # 输出分类报告和混淆矩阵
 print('分类报告:')
 print(classification_report(y_test, y_pred1, target_names=label_encoder.classes_))
 print('混淆矩阵:')
 print(confusion_matrix(y_test, y_pred1))
-
-# 训练SVM模型
-svm_model = SVC(kernel='precomputed')
-svm_model.fit(X_train, y_train)
-print("7")
-# 进行预测
-y_pred2 = svm_model.predict(X_test)
-
-# 输出分类报告和混淆矩阵
-print('分类报告:')
-print(classification_report(y_test, y_pred2, target_names=label_encoder.classes_))
-print('混淆矩阵:')
-print(confusion_matrix(y_test, y_pred2))
-
-# 训练SVM模型
-svm_model = SVC(kernel='poly')
-svm_model.fit(X_train, y_train)
-print("7")
-# 进行预测
-y_pred3 = svm_model.predict(X_test)
-
-# 输出分类报告和混淆矩阵
-print('分类报告:')
-print(classification_report(y_test, y_pred3, target_names=label_encoder.classes_))
-print('混淆矩阵:')
-print(confusion_matrix(y_test, y_pred3))
-
-# 训练SVM模型
-svm_model = SVC(kernel='sigmoid')
-svm_model.fit(X_train, y_train)
-print("7")
-# 进行预测
-y_pred4 = svm_model.predict(X_test)
-
-# 输出分类报告和混淆矩阵
-print('分类报告:')
-print(classification_report(y_test, y_pred4, target_names=label_encoder.classes_))
-print('混淆矩阵:')
-print(confusion_matrix(y_test, y_pred4))
-
